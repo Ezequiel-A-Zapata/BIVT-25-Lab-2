0dedadbda89bba59b29ac04e046097a5e4a0b681
Ezequiel Alejandro Zapata <157446504+Ezequiel-A-Zapata@users.noreply.github.com>
2025-10-05 11:05:21 +0300

    Update Blue.cs

--- Lab2/Blue.cs
+++ Lab2/Blue.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 
 namespace Lab2

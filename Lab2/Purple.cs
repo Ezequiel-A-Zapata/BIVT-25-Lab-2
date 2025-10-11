251df341dbd3fe99887f1726875971b0915d9582
Ezequiel Alejandro Zapata <157446504+Ezequiel-A-Zapata@users.noreply.github.com>
2025-10-11 14:43:48 +0300

    Update Purple.cs

--- Lab2/Purple.cs
+++ Lab2/Purple.cs
@@ -1,95 +1,96 @@
-// ﻿using System.Collections.Generic;
-// using System.ComponentModel;
-// using System.Runtime.InteropServices;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
 
-// namespace Lab2
-// {
-//     public class Purple
-//     {
-//         const double E = 0.0001;
-//         public int Task1(int n, int p, int h)
-//         {
-//             int answer = 0;
+namespace Lab2
+{
+    public class Purple
+    {
+        const double E = 0.0001;
+        public int Task1(int n, int p, int h)
+        {
+            int answer = 0;
 
-//             // code here
+            // code here
 
-//             // end
+            // end
 
-//             return answer;
-//         }
-//         public (int quotient, int remainder)  Task2(int a, int b)
-//         {
-//             int quotient = 0;
-//             int remainder = 0;
+            return answer;
+        }
+        public (int quotient, int remainder)  Task2(int a, int b)
+        {
+            int quotient = 0;
+            int remainder = 0;
 
-//             // code here
+            // code here
 
-//             // end
+            // end
 
-//             return (quotient, remainder);
-//         }
-//         public double Task3()
-//         {
-//             double answer = 0;
+            return (quotient, remainder);
+        }
+        public double Task3()
+        {
+            double answer = 0;
 
-//             // code here
+            // code here
 
-//             // end
+            // end
 
-//             return answer;
-//         }
-//         public int Task4(double b, double q)
-//         {
-//             int answer = 0;
+            return answer;
+        }
+        public int Task4(double b, double q)
+        {
+            int answer = 0;
 
-//             // code here
+            // code here
 
-//             // end
+            // end
 
-//             return answer;
-//         }
-//         public int Task5(int a, int b)
-//         {
-//             int answer = 0;
+            return answer;
+        }
+        public int Task5(int a, int b)
+        {
+            int answer = 0;
 
-//             // code here
+            // code here
 
-//             // end
+            // end
 
-//             return answer;
-//         }
-//         public long Task6()
-//         {
-//             long answer = 0;
+            return answer;
+        }
+        public long Task6()
+        {
+            long answer = 0;
 
-//             // code here
+            // code here
 
-//             // end
+            // end
 
-//             return answer;
-//         }
+            return answer;
+        }
 
-//         public int Task7(double S, double d)
-//         {
-//             int answer = 0;
+        public int Task7(double S, double d)
+        {
+            int answer = 0;
 
-//             // code here
+            // code here
 
-//             // end
+            // end
 
-//             return answer;
-//         }
-//         public (double SS, double SY) Task8(double a, double b, double h)
-//         {
-//             double SS = 0;
-//             double SY = 0;
+            return answer;
+        }
+        public (double SS, double SY) Task8(double a, double b, double h)
+        {
+            double SS = 0;
+            double SY = 0;
 
-//             // code here
+            // code here
 
-//             // end
+            // end
 
-//             return (SS, SY);
-//         }
-//     }
+            return (SS, SY);
+        }
+    }
 
-// }
+}
+
